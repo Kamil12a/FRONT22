ae634c28c0dd0f8fa1d9351e30bf2f56070ef783
kamilPlewka <kamilplewka12a@wp.pl>
2022-09-07 12:15:45 +0200

    wip

---
+++ Frontend/my-app/src/Pages/YourGroups/components/YourGroupsPanel/YourGroupsPanel.js
@@ -0,0 +1,25 @@
+import GroupLook from "../../../Groups/Components/GroupLook";
+import { useNavigate } from "react-router-dom";
+import Navigation from "../../../../Navigation";
+function YourGroupsPanel() {
+  let navigate = useNavigate();
+
+  const functionOnClickBtn=()=>{
+    navigate("/")
+  }
+  return (
+    <>
+   
+    <Navigation/>
+    <GroupLook
+      buttonText={"Check Details"}
+      description={"xd"}
+      id={1}
+      groupOwnerId={"xd"}
+      functionOnClickBtn={functionOnClickBtn}
+    />
+     </>
+  );
+}
+
+export default YourGroupsPanel;
